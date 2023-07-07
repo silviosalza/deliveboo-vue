b383c36ab273d00b0fcce218694aed49716936cf
simolorevg <simoneviaggi@libero.it>
2023-07-07 11:41:58 +0200

    Redoo the work into the new frontend

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,25 @@
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
 
-export default{
-    components: { AppHeader, AppFooter, }
+export default {
+  components: { AppHeader, AppFooter, },
+  data() {
+    return {
+      store
+    }
   }
+}
 
 </script>
 
 <template>
-  <AppHeader/>
+  <AppHeader />
 
   <router-view></router-view>
-  <AppFooter/>
-
+  <AppFooter />
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
 @use "./styles/utilities/variables" as *;
-
-
-
 </style>
