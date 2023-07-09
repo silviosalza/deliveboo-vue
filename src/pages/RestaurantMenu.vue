01f70b8dab38ddb0c8f7f87ff6c3d5f92f38aa67
silviosalza <silviofilipposalza@gmail.com>
2023-07-09 11:49:15 +0200

    fix cart code

--- src/pages/RestaurantMenu.vue
+++ src/pages/RestaurantMenu.vue
@@ -9,15 +9,16 @@ export default {
             dishesArray: [],
             myUrl: 'http://localhost:8000',
 
+
         }
     },
     methods: {
         updateStore(data) {
             let divElement = document.getElementById(`${data}`);//punto di riferimento è l'id della card che sarà l'id del piatto
             let dishTitle = divElement.querySelector('#dish-title').innerHTML;//dalla card ricavo il nome del piatto
+            let dishPrice = parseFloat(divElement.querySelector('#dish-price').innerHTML);//e il prezzo
             let dishId = divElement.querySelector('#dish-id').innerHTML;//dalla card ricavo id del piatto
             let dishRestaurantId = divElement.querySelector('#dish-restaurant-id').innerHTML;//dalla card ricavo id del ristorante
-            let dishPrice = parseFloat(divElement.querySelector('#dish-price').innerHTML);//e il prezzo
             let dishObject = {//dopodicchè creo un oggetto con nome del piatto, il suo prezzo e un counter che mi servirà
                 name: dishTitle,//per contare quante volte è stato inserito nel carrello un singolo piatto
                 price: dishPrice,
@@ -25,31 +26,26 @@ export default {
                 restaurant_id: dishRestaurantId,
                 dish_id: dishId
             }
-            if (this.store.cartArray.some(item => item.name === dishTitle)) {//se nel carrello è già presente il piatto
-                let findItems = this.store.cartArray.filter(item => item['name'] === dishTitle);//cerco l'oggetto che ha come nome dishTitle (findItems risulterà un array)
-                let itemFounded = findItems[0];//lo assegno alla variabile itemFounded
-                itemFounded.count += 1;//incremento di uno il count dell'elemento
-                findItems = [];//svuoto l'array
+            if (this.store.cartArray.length === 0) {
+                this.store.cartArray.push(dishObject);//lo inserisco nell'array
+                this.store.totalProducts += 1;//incremento i prodotti presi di uno
+                this.store.totalPrice = this.store.totalPrice + dishPrice;//calcola il totale
+
             } else {//altrimenti
 
-                if(this.store.cartArray.length === 0){
+                if (this.store.cartArray.some(item => item.name === dishTitle)) {
+                    //se nel carrello è già presente il piatto
+                    let findItems = this.store.cartArray.filter(item => item['name'] === dishTitle);//cerco l'oggetto che ha come nome dishTitle (findItems risulterà un array)
+                    let itemFounded = findItems[0];//lo assegno alla variabile itemFounded
+                    itemFounded.count += 1;//incremento di uno il count dell'elemento
+                    findItems = [];//svuoto l'array
+                    this.store.totalPrice = this.store.totalPrice + dishPrice;//calcola il totale
+
+                } else if (this.store.cartArray[0].restaurant_id === dishObject.restaurant_id) {
                     this.store.cartArray.push(dishObject);//lo inserisco nell'array
                     this.store.totalProducts += 1;//incremento i prodotti presi di uno
                     this.store.totalPrice = this.store.totalPrice + dishPrice;//calcola il totale
-
-                } else if(this.store.cartArray[0].restaurant_id === dishObject.restaurant_id ){
-                    this.store.cartArray.push(dishObject);//lo inserisco nell'array
-                    this.store.totalProducts += 1;//incremento i prodotti presi di uno
-                    this.store.totalPrice = this.store.totalPrice + dishPrice;//calcola il totale
-
-                // } else if(this.store.cartArray.some(item => item.dish_id === dishObject.dish_id)){
-
-                //     this.store.cartArray.push(dishObject);//lo inserisco nell'array
-                //     this.store.totalPrice = this.store.totalPrice + dishPrice;//calcola il totale
-                //     this.store.totalProducts += 1;//incremento i prodotti presi di uno
-
-                // }
-                }else{
+                } else {
                     alert('Non puoi aggiungere prodotti di un ristorante diverso')
                 }
             }
@@ -81,8 +77,7 @@ export default {
     },
     mounted() {
         const id = this.$route.params.id
-        if (id){
-
+        if (id) {
             this.getDishes(id)
         }
     }
@@ -97,7 +92,7 @@ export default {
         <button class="btn btn-primary" @click="updateStore(1)">Test Bottone</button>
     </div> -->
 
-    <div v-for="(product, index) in dishesArray" :key="index" class="card" :id="product.id " style="width: 18rem;">
+    <div v-for="(product, index) in dishesArray" :key="index" class="card" :id="product.id" style="width: 18rem;">
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
             <h5 id="dish-title" class="card-title">{{ product.dish_name }}</h5>
