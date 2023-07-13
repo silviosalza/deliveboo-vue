2808d2ec8181c527c203ffade70cf14caabc6e46
simolorevg <simoneviaggi@libero.it>
2023-07-13 18:23:21 +0200

    fixed some style withy thankyou page

--- src/pages/Thanks.vue
+++ src/pages/Thanks.vue
@@ -1,30 +1,34 @@
-  <script>
+<script>
 import methods from 'braintree-web/lib/methods';
 
-  export default {
-    name: 'Thanks',
-    mounted(){
-        setTimeout(() => {
-            this.$router.push('/');
-        }, 4000);
+export default {
+  name: 'Thanks',
+  mounted() {
+    setTimeout(() => {
+      this.$router.push('/');
+    }, 4000);
 
-    }
-
-    
   }
+
+
+}
 
 </script>
 
 <template>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 offset-md-3 text-center">
-          <h1 class="mb-4">Grazie per l'acquisto, passa a trovarci presto</h1>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 text-center d-flex flex-column justify-content-center align-items-center">
+        <h1 class="mb-4">Grazie per l'acquisto e buon appetito!</h1>
+        <img src="src\img\delivering.gif" alt="Delivery Guy">
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   
-  <style>
-  </style>
+<style lang="scss" scoped>
+img {
+  max-width: 200px;
+}
+</style>
